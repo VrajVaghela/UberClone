34bcc5cd58b790c46646c3d0fcf7c5459a8a98b4
VrajVaghela <vrjvghl@gmail.com>
2025-05-24 18:26:09 +0530

    Refactor user model for improved readability and maintainability

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -8,11 +8,11 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true,
             minlength: [3, 'First name must be at least 3 characters long']
-        }
-    },
-    lastname: {
-        type: String,
-        minlength: [3, 'Last name must be at least 3 characters long']
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must be at least 3 characters long']
+        },
     },
     email:{
         type: String,
